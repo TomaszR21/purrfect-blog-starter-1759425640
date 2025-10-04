84283aaf311f75eeb5f4051eec1cd63895dc4a1d
Tomasz Romanowski <tomeqromanowski21@gmail.com>
2025-10-04 19:26:17 +0200

    Initial Page

---
+++ purrfect-blog-starter-1759425640/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace purrfect_blog_starter_1759425640
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ purrfect-blog-starter-1759425640/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="purrfect_blog_starter_1759425640.MvcApplication" Language="C#" %>
